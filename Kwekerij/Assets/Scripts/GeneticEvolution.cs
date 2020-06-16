4d8bb15cc10af091727c72e373a63de9fd0fd559
Josh <joshmathew.va@gmail.com>
2020-06-16 14:27:12 +0200

    First Commit
    o Edited premade gitignore to ignore Library and LIbrary Database folders
    o Updated ignore to actually ignore local folders
    + Added the GeneticEvolution script

---
+++ Kwekerij/Assets/Scripts/GeneticEvolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneticEvolution : MonoBehaviour
+{
+    //Chromosome (string): Solution
+    //Gene (bits): Part of solution
+    //Locus: Poisition of gene
+    //Alleles: Value of gene
+    //Phenotype: Decoded solution
+    //Genotype: Encoded solution
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
