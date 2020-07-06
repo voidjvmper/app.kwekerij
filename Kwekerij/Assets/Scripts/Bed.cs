550e4e610999e915da55a84705eb99e3fb9d8e54
Josh <joshmathew.va@gmail.com>
2020-07-06 06:20:07 +0200

    App Framework
    + Added Garden, Bed, Patch, Plant and Soil classes which govern how the garden works

---
+++ Kwekerij/Assets/Scripts/Bed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VUSSK_GeneticEvolution;
+
+public class Bed : GeneticEntity
+{
+    private Patch[] patches = null;
+    public Bed(int[] pChromosome, float pFitness, Patch[] pPatches) : base(pChromosome, pFitness)
+    {
+        patches = pPatches;
+    }
+    [SerializeField] private int seed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Kwekerij/Assets/Scripts/Soil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soil : MonoBehaviour
+{
+    [Range(0, 100)]
+    [SerializeField] private float clayPercentage;
+    [Range(0, 100)]
+    [SerializeField] private float siltPercentage;
+    [Range(0, 100)]
+    [SerializeField] private float sandPercentage;
+    [Range(0, 14)]
+    [SerializeField] private float soil_pH;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
